b4d0ce54d01e5a6ab0ec62fae211ea1bca3d4670
benetonmari <143605866+benetonmari@users.noreply.github.com>
2024-10-29 08:54:51 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-function criaCartao(categoria,pergunta,resposta){
+function criaCartao(categoria, pergunta, resposta) {
    let container = document.getElementById('container')
    let article = document.createElement('article')
    article.className = 'cartao'
-   article.innerHTML = 'isso éum teste'
+   article.innerHTML = `
+   <div class='cartao-conteudo'>
+      <h3> ${categoria} </h3>
+   <div class='cartao-conteudo-pergunta'>
+      <p> ${pergunta} </p>
+   </div>
+   <div class='cartao-conteudo-resposta'>
+      <p> ${resposta} </p>
+   </div>
+   </div>
+`
+
    container.appendChild(article)
 
 }
 
-criaCartao("Biologia","Qual a função da mitocondria","Produção de ATP")
+criaCartao("Biologia", "Qual a função da mitocondria", "Produção de ATP")
+criaCartao('Matematica', 'Como é definido grupo?','É um conjunto unido de uma operação onde esta é fechada no conjunto e tem algumas propriedases')
